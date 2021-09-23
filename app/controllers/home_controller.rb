3bff84eb841bc72c5ea7ace59dfbd78b89a0f58b
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-09-23 18:32:00 +0200

    carousel home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all.first(2)
-    @wishlist = Product.all.last(2)
+    @products = Product.all.first(5)
+    @wishlist = Product.all.last(5)
   end
 end
