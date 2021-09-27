d33a8b978523b2b741ebd0baf168174b541593b1
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-09-27 16:20:11 +0200

    fix

--- app/controllers/compte_controller.rb
+++ app/controllers/compte_controller.rb
@@ -1,4 +1,9 @@
 class CompteController < ApplicationController
   def evenements
+    @participations = Participant.all.find_all { |participant| participant.user_id == 3}
+  end
+
+  def commandes
+    @commandes = Order.all.find_all { |order| order.user_id == 3}
   end
 end
