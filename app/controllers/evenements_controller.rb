4c725021a0a387ec464ab7f12b5b88db3ae63086
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-09-28 18:25:06 +0200

    divers

--- app/controllers/evenements_controller.rb
+++ app/controllers/evenements_controller.rb
@@ -5,16 +5,42 @@ class EvenementsController < ApplicationController
 
   def evenement
     @event = Event.find_by_id(request.query_parameters[:id])
-    @participations = Participant.all.find_all { |participant| participant.user_id == current_user.id}
-    @participations.each do |participe|
-      if participe.event_id == @event.id
-        @participe = true
-        break
+    if current_user != nil
+      @participations = Participant.all.find_all { |participant| participant.user_id == current_user.id}
+      @participations.each do |participe|
+        if participe.event_id == @event.id
+          @participe = true
+          break
+        end
       end
     end
     @participant = Participant.new
     @participant.event_id = @event.id
-    @participant.user_id = current_user.id
     @placeRestantes = @event.places - @event.participants.length
+  end
+
+  def create
+    @participant = Participant.new(participant_params) 
+    @event = @participant.event
+    @placeRestantes = @event.places - @event.participants.length
+    @participant.user_id = current_user.id
+
+    if @placeRestantes > 0
+    redirect_link = evenements_path + "/evenement?id=" + @event.id.to_s
+    respond_to do |format|
+      if @participant.save
+        format.html { redirect_to redirect_link, notice: "Vous avez été inscrit à l'évènement." }
+        format.json { render :show, status: :created, location: @participant }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @participant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  end
+
+
+  def participant_params
+    params.require(:participant).permit(:id, :event_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :buys
   resources :categories
   resources :products
+  resources :evenements
   #get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
