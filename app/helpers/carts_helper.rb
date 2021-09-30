d5518d55265404a5d02c201ad5bbf837ec9519c4
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-09-30 16:45:25 +0200

    panier, total, design,...

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,9 +1,11 @@
 module CartsHelper
-        def sub_total
+        def sub_total(cart)
             sum = 0
-            :line_items.each do |line_item|
-              sum+= line_item.total_price
+            p cart
+            cart.line_items.each do |line_item|
+              sum+= line_item.quantity * line_item.product.price
             end
             return sum
         end
+        module_function :sub_total
 end
