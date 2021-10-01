a11d017bf4a962f65c969fe2e8e7dcb904da394b
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-01 17:06:06 +0200

    migrate fix

--- db/migrate/20210929160241_create_line_items.rb
+++ db/migrate/20210929160241_create_line_items.rb
@@ -4,8 +4,6 @@ class CreateLineItems < ActiveRecord::Migration[6.1]
       t.integer  "quantity",   default: 1
       t.integer  "product_id"
       t.integer  "cart_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
       t.integer  "order_id"
       t.timestamps
     end
