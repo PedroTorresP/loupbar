7f7a987373863ff66d0445ee252a4476eec74d1d
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-14 18:15:28 +0200

    customisation insert fix

--- db/migrate/20211014140517_add_customisations.rb
+++ db/migrate/20211014140517_add_customisations.rb
@@ -1,9 +1,9 @@
 class AddCustomisations < ActiveRecord::Migration[6.1]
   def up
-    execute "insert into customisations (name, created_at, updated_at) values('background', '0000-00-00 00:00:00', '0000-00-00 00:00:00')"
-    execute "insert into customisations (name, created_at, updated_at) values('character1', '0000-00-00 00:00:00', '0000-00-00 00:00:00')"
-    execute "insert into customisations (name, created_at, updated_at) values('character2', '0000-00-00 00:00:00', '0000-00-00 00:00:00')"
-    execute "insert into customisations (name, created_at, updated_at) values('character3', '0000-00-00 00:00:00', '0000-00-00 00:00:00')"
-    execute "insert into customisations (name, created_at, updated_at) values('character4', '0000-00-00 00:00:00', '0000-00-00 00:00:00')"
+    execute "insert into customisations (name, created_at, updated_at) values('background', '2021-09-13 16:07:50.905390000 +0000', '2021-09-13 16:07:50.905390000 +0000')"
+    execute "insert into customisations (name, created_at, updated_at) values('character1', '2021-09-13 16:07:50.905390000 +0000', '2021-09-13 16:07:50.905390000 +0000')"
+    execute "insert into customisations (name, created_at, updated_at) values('character2', '2021-09-13 16:07:50.905390000 +0000', '2021-09-13 16:07:50.905390000 +0000')"
+    execute "insert into customisations (name, created_at, updated_at) values('character3', '2021-09-13 16:07:50.905390000 +0000', '2021-09-13 16:07:50.905390000 +0000')"
+    execute "insert into customisations (name, created_at, updated_at) values('character4', '2021-09-13 16:07:50.905390000 +0000', '2021-09-13 16:07:50.905390000 +0000')"
   end
 end
