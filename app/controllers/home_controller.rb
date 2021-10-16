2e17e5f819960d964f3d3607779a1bdb5089ad2d
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-16 21:03:56 +0200

    evenements page accueil + small fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   def index
     @products = Product.where('DATE(date) <= ?', Date.today).order(date: :desc).first(5)
     @wishlist = Product.all.last(5)
+    @events = Event.all.select { |e| e.date >= Date.today.beginning_of_week && e.date <= Date.today.end_of_week }
+    p "--------------"
+    p @events
   end
 
 end
