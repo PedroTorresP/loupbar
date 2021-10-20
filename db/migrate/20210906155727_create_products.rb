f88c00ba52ab60cfbd1fe61af55cafb265e5febb
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-20 20:03:56 +0200

    db fix

--- db/migrate/20210906155727_create_products.rb
+++ db/migrate/20210906155727_create_products.rb
@@ -2,13 +2,13 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.integer :category
       t.string :description
       t.decimal :price
       t.string :image_path
       t.integer :quantity
       t.integer :min_stock
       t.boolean :available
+      t.integer :subcategory_id
 
       t.timestamps
     end
--- db/migrate/20211020162714_add_subcategory_to_product.rb
+++ db/migrate/20211020162714_add_subcategory_to_product.rb
@@ -1,6 +1,5 @@
 class AddSubcategoryToProduct < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :subcategory_id, :integer
     add_index :products, :subcategory_id
   end
 end
