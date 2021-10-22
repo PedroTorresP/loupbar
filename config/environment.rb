4c59ed9d6aaef213a8f9342767f3198e08ad0916
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-22 21:52:14 +0200

    fix faute frappe

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    password: Rails.application.credentials.dig(:sengrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
+    password: Rails.application.credentials.dig(:sendgrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
     domain: 'loupbar.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
