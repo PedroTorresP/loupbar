19cd3eed0cae1b618a3b54a0a39bebd7c13ddcb8
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-10-29 23:39:36 +0200

    for config

--- app/helpers/customisations_helper.rb
+++ app/helpers/customisations_helper.rb
@@ -9,7 +9,7 @@ module CustomisationsHelper
     end
 
     def get_value(name)
-        return Customisation.all.find_by_name(name).value
+        return ''#Customisation.all.find_by_name(name).value
     end
 
     module_function :get_image
