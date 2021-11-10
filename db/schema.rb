29e0b6a54e169d58a05d56d31633ba217fee2f83
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-11-10 19:13:43 +0100

    ville

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_230636) do
+ActiveRecord::Schema.define(version: 2021_11_10_175833) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_230636) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
