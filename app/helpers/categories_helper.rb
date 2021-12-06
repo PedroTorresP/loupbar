bf00029c9a601c0f7626b856bdcede1737b04f5d
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2021-12-06 19:07:19 +0100

    order by name

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,19 +2,12 @@ module CategoriesHelper
 
     def get_categories
         products = Product.all.select { |product| product.available || product.quantity > 0  }
-        categories = Category.where.not(id: 1)
-        subcategories = Subcategory.where.not(id: 1)
-        lstCategories = {}
-        p '-------------------------------------------------------------------->'
-        categories.each do |category|
-            lst = Product.where(category_id: category.id).pluck(:subcategory_id).uniq
-        end
-        p lstCategories
+        categories = Category.where.not(id: 1).sort_by(&:name)
         return categories
     end
 
     def get_subcategories(category)
-        return Product.where(category_id: category.id).pluck(:subcategory_id).uniq
+        return Product.where(category_id: category.id).sort_by(&:name).pluck(:subcategory_id).uniq
     end
 
     module_function :get_categories
