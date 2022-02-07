53b281053aff689714ecabf0b3d97e08bcbb114b
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-02-07 14:36:25 +0100

    modification évènements du mois et tag sélection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.where('DATE(date) <= ?', Date.today).order(date: :desc).first(5)
+    @products = Product.where('DATE(date) <= ?', Date.today).order(date: :desc).first(3)
+    @products += Product.select{ |product| product.description.include?('#selection') }
     @wishlist = Product.all.last(5)
-    @events = Event.all.select { |event| event.date >= Date.today.beginning_of_week && event.date <= Date.today.end_of_week }
-    p "--------------"
-    p @events
+    @events = Event.all.select { |event| event.date >= Date.today && event.date <= Date.today.end_of_month }
+    @events = @events.sort_by{|event| event.date}
   end
 
 end
