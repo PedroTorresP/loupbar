80da86f2b49eb3b77216d9518f92664a08c519cb
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-02-15 16:56:45 +0100

    exporter en csv pour facebook

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,5 +4,6 @@ module ProductsHelper
         nb_low_stock += Product.all.select { |product| product.quantity.to_i < product.min_stock.to_i && product.available  }.length
         return nb_low_stock
     end
+
     module_function :low_stock_count
 end
