c7f29fddda9ec9862241a2df48e357de39cde369
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-02-24 16:32:30 +0100

    product home to 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.where('DATE(date) <= ?', Date.today).order(date: :desc).first(3)
+    @products = Product.where('DATE(date) <= ?', Date.today).order(date: :desc).first(5)
     @products += Product.select{ |product| product.description.include?('#selection') }
     @wishlist = Product.all.last(5)
     @events = Event.all.select { |event| event.date >= Date.today && event.date <= Date.today.end_of_month }
