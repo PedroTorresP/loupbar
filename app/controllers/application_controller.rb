0b69cf29013b6b8932050f227ae4739352cf216f
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-05-31 17:44:38 +0200

    IP addresses on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :update_ip
+    before_action :block_ip_addresses
     include ApplicationHelper
 
     protected
@@ -7,6 +9,21 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :first_name, :last_name, :postal_code, :city, :phone])
         devise_parameter_sanitizer.permit(:account_update, keys: [:address, :first_name, :last_name, :postal_code, :city, :phone])
+    end
+
+    def update_ip
+        if current_user != nil
+            current_user.ip = current_ip_address
+            current_user.save
+        end
+    end
+    
+    def block_ip_addresses
+        #head :unauthorized if current_ip_address == "XX.XX.XX.XX"
+    end
+  
+    def current_ip_address
+      request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
     end
 
 end
---
+++ db/migrate/20220531153634_user_ip.rb
@@ -0,0 +1,5 @@
+class UserIp < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :ip, :string
+  end
+end
