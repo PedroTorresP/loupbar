fc67ef5952ccfe81184ba28c489c3705ad75f1b9
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-07-02 18:25:34 +0200

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,5 @@
 module ApplicationHelper
     def session_cart
-                #detruire les anciens paniers après 1 semaine
-                oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.week.ago) }
-                if oldCarts.length > 100
-                    oldCarts.each do |cart|
-                        cart.destroy
-                    end
-                end
-        
-                #detruire les paniers vide après 1 jour
-                oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.day.ago) }
-                if oldCarts.length > 100
-                    emptyCartsIds = oldCarts.pluck(:id) - LineItem.pluck(:cart_id)
-                    if emptyCartsIds.length > 100
-                        emptyCarts = emptyCartsIds.find_all{|c| emptyCartsIds.include?c.id}
-                        emptyCarts.each do |cart|
-                            cart.destroy
-                        end
-                    end
-                end
         Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
         #créer un nouveau panier
@@ -28,3 +9,23 @@ module ApplicationHelper
     end
     module_function :session_cart
 end
+
+                #detruire les anciens paniers après 1 semaine
+#                oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.week.ago) }
+#                if oldCarts.length > 100
+ #                   oldCarts.each do |cart|
+  #                      cart.destroy
+   #                 end
+  #              end
+        
+                #detruire les paniers vide après 1 jour
+   #             oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.day.ago) }
+    #            if oldCarts.length > 100
+     #               emptyCartsIds = oldCarts.pluck(:id) - LineItem.pluck(:cart_id)
+      #              if emptyCartsIds.length > 100
+       #                 emptyCarts = emptyCartsIds.find_all{|c| emptyCartsIds.include?c.id}
+        #                emptyCarts.each do |cart|
+         #                   cart.destroy
+          #              end
+           #         end
+            #    end
