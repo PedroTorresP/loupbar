606d4a781f3419afcdc6f78381e4e4e982906b96
Gabriel Pacheco <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2022-07-02 18:21:19 +0200

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
 module ApplicationHelper
     def session_cart
+                #detruire les anciens paniers après 1 semaine
+                oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.week.ago) }
+                if oldCarts.length > 100
+                    oldCarts.each do |cart|
+                        cart.destroy
+                    end
+                end
+        
+                #detruire les paniers vide après 1 jour
+                oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.day.ago) }
+                if oldCarts.length > 100
+                    emptyCartsIds = oldCarts.pluck(:id) - LineItem.pluck(:cart_id)
+                    if emptyCartsIds.length > 100
+                        emptyCarts = emptyCartsIds.find_all{|c| emptyCartsIds.include?c.id}
+                        emptyCarts.each do |cart|
+                            cart.destroy
+                        end
+                    end
+                end
         Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
-        #detruire les anciens paniers après 1 semaine
-        oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.week.ago) }
-        if oldCarts.length > 100
-            oldCarts.each do |cart|
-                cart.destroy
-            end
-        end
-
-        #detruire les paniers vide après 1 jour
-        oldCarts = Cart.all.find_all {|cart| cart.created_at.before?(1.day.ago) }
-        if oldCarts.length > 100
-            emptyCartsIds = oldCarts.pluck(:id) - LineItem.pluck(:cart_id)
-            if emptyCartsIds.length > 100
-                emptyCarts = emptyCartsIds.find_all{|c| emptyCartsIds.include?c.id}
-                emptyCarts.each do |cart|
-                    cart.destroy
-                end
-            end
-        end
         #créer un nouveau panier
         cart = Cart.create
         session[:cart_id] = cart.id
